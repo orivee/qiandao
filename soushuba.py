6cdc398d7609256f74314dbeabef4b29c36e7120
violet <violet@onevoid.me>
2023-05-04 21:44:05 +0800

    更新搜书吧签到

--- soushuba.py
+++ soushuba.py
@@ -2,6 +2,7 @@
 """
 实现搜书吧论坛登入和发布空间动态
 """
+import os
 import re
 import sys
 from copy import copy
@@ -10,8 +11,6 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import logging
-
-import config as cfg
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -25,19 +24,23 @@ logger.addHandler(ch)
 
 class SouShuBaClient:
 
-    def __init__(self, hostname: str, proxies: dict | None = None):
+    def __init__(self, hostname: str, username: str, password: str, questionid: str = '0', answer: str = None,
+                 proxies: dict | None = None):
         self.session: requests.Session = requests.Session()
         self.hostname = hostname
+        self.username = username
+        self.password = password
+        self.questionid = questionid
+        self.answer = answer
         self._common_headers = {
             "Host": f"{ hostname }",
             "Connection": "keep-alive",
-            "Accept": "*/*",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
             "Accept-Language": "zh-CN,cn;q=0.9",
+            "Content-Type": "application/x-www-form-urlencoded",
         }
         self.proxies = proxies
-
-        self.username = None
 
     def login_form_hash(self):
         rst = self.session.get(f'https://{self.hostname}/member.php?mod=logging&action=login').text
@@ -45,10 +48,12 @@ class SouShuBaClient:
         formhash = re.search(r'<input type="hidden" name="formhash" value="(.+?)" />', rst).group(1)
         return loginhash, formhash
 
-    def login(self, username: str, password: str, questionid: str = '0', answer: str = None):
+    def login(self, ):
         """Login with username and password"""
         loginhash, formhash = self.login_form_hash()
-        login_url = f'https://{self.hostname}/member.php?mod=logging&action=login&loginsubmit=yes&loginhash={loginhash}&inajax=1'
+        login_url = f'https://{self.hostname}/member.php?mod=logging&action=login&loginsubmit=yes' \
+                    f'&handlekey=register&loginhash={loginhash}&inajax=1'
+
 
         headers = copy(self._common_headers)
         headers["origin"] = f'https://{self.hostname}'
@@ -56,18 +61,15 @@ class SouShuBaClient:
         payload = {
             'formhash': formhash,
             'referer': f'https://{self.hostname}/',
-            'loginfield': username,
-            'username': username,
-            'password': password,
-            'questionid': questionid,
-            'answer': answer,
-            'cookietime': 2592000
+            'username': self.username,
+            'password': self.password,
+            'questionid': self.questionid,
+            'answer': self.answer
         }
 
         resp = self.session.post(login_url, proxies=self.proxies, data=payload, headers=headers)
-        if resp.status_code == 200 and self.session.cookies.get('yj0M_af16_auth'):
-            self.username = username
-            logger.info(f'{username} 登录成功!\n20%')
+        if resp.status_code == 200 and self.session.cookies.get('yj0M_a233_auth'):
+            logger.info(f'Welcome {self.username}!')
         else:
             raise ValueError('Verify Failed! Check your username and password!')
 
@@ -92,7 +94,6 @@ class SouShuBaClient:
         headers = copy(self._common_headers)
         headers["origin"] = f'https://{self.hostname}'
         headers["referer"] = f'https://{self.hostname}/'
-        headers["Content-Type"] = 'application/x-www-form-urlencoded'
 
         for x in range(5):
             payload = {
@@ -104,20 +105,20 @@ class SouShuBaClient:
             }
             resp = self.session.post(space_url, proxies=self.proxies, data=payload, headers=headers)
             if re.search("操作成功", resp.text):
-                logger.info(f'{self.username} 发布第 {x + 1} 次空间动态成功!\n{int(100 * (3 + x) / 10)}%')
+                logger.info(f'{self.username} post {x + 1}nd successfully!')
                 time.sleep(120)
             else:
-                raise ValueError(f'{self.username} 发布第 {x + 1} 次空间动态失败!')
+                logger.warning(f'{self.username} post {x + 1}nd failed!')
 
 
 if __name__ == '__main__':
     try:
-        client = SouShuBaClient('www.feb.sbooook.com')
-        client.login(cfg.username, cfg.password)
+        client = SouShuBaClient(os.environ.get('SOUSHUBA_HOSTNAME', 'www.apr.soushu2029.com'),
+                                os.environ.get('SOUSHUBA_USERNAME'),
+                                os.environ.get('SOUSHUBA_PASSWORD'))
+        client.login()
         client.space()
         credit = client.credit()
-        logger.info('{0}\n100%\n{{ "complete": 1, "code": 0, "description": "{0}" }}'.format(
-            f"{cfg.username} 现在拥有 {credit} 枚银币。"))
     except Exception as e:
         logger.error(e)
         logger.info('{0}\n100%\n{{ "complete": 0, "code": 1, "description": "{0}" }}'.format(e))
